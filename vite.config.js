97694631bcc7b18031c527eac7d173b22189ab4f
Dirk <45898176+dvkcds@users.noreply.github.com>
2025-03-03 12:32:15 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Time-Tracking-Dashboard', // Zorgt ervoor dat assets relatief worden geladen
+  base: '/Time-Tracking-Dashboard/', // Zorgt ervoor dat assets relatief worden geladen
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: 'index.html', // 👈 Dit zorgt ervoor dat Vite index.html verwerkt!
+    },
+  },
 })
